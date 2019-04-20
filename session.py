693653987535f1a95aeacdf507cccf9cae3923d3
Connor Oswold <coswoldo@gmail.com>
2019-04-20 16:44:09 -0700

    cleaned up but still needs refactoring

--- session.py
+++ session.py
@@ -10,27 +10,35 @@ class Session(object):
         self.subject = input('Enter the subject you\'re working on today: ')
         self.pomodoro = 0
         self.hours = 0.0
+        self.short_dia = ['Grab some water.', 'Stand and stretch.',
+        'Make a tea.', 'Eat a fruit.', 'Draw a small doodle.',
+        'Text a friend to have a good day.', 'Text your mom.',
+        'Practice gratitude: write down what you\'re thankful for right now.',
+        'Mini-meditation.', 'Visualize nice things.',
+        'Listen to music and take a quick stroll.']
+        self.long_dia = ['Walk to the park.', 'Go get a coffee.', 'Meditate.',
+         'Ask someone to to go on a walk.', 'Get a pastry and walk around.',
+         'Play ping-pong.', 'Draw.', 'Close your eyes.']
 
     def print_update(self):
         """Print Current pomodoro's and subject."""
         i = 0
         print("\033[H\033[J")
-        print('Keep focused on: ' + self.subject)
-        print('You\'ve been focused for ' + str(self.hours) + ' hours.')
+        print('Keep focused on: ' + '\x1b[36m' + self.subject + '\x1b[0m')
+        print('You\'ve been focused for ' + '\x1b[35m' + str(self.hours) +
+        ' hours' + '\x1b[0m')
         while i < self.pomodoro:
-            print('|', end='')
+            print('\x1b[33m' + '|' + '\x1b[0m', end='')
             i += 1
         print('')
 
     def print_dialogue(self, tim):
         if tim == 0:
-            random = random.randint(0,len(short_dia) - 1)
-            print('Take a short break!')
-            messagebox.showinfo("Break time!", "5 minute break: " + short_dia[random])
+            num = random.randint(0,len(self.short_dia) - 1)
+            messagebox.showinfo("Break time!", "5 minute break: " + self.short_dia[num])
         else:
-            random = random.randint(0,len(long_dia) - 1)
-            print('Take a long break!')
-            messagebox.showinfo("Break time!", "30 minute break: " + long_dia[random])
+            num = random.randint(0,len(self.long_dia) - 1)
+            messagebox.showinfo("Break time!", "30 minute break: " + self.long_dia[num])
 
     def rest(self, tim):
         self.print_dialogue(tim)
@@ -47,7 +55,7 @@ class Session(object):
         else:
             self.rest(0)
         self.print_update()
-        return self.timer()
+        self.timer()
 
     def timer(self):
         """Keep track of 25 minute intervals"""
@@ -65,4 +73,4 @@ class Session(object):
                     seconds = 0
             except KeyboardInterrupt:
                 raise KeyError('You have finished the session.')
-        return self.update()
+        self.update()
