1ac75d4bdd4ad12dd3c2fc8dc91aef99d366cfe5
Connor Oswold <coswoldo@gmail.com>
2019-04-20 16:14:05 -0700

    implemented tkinter

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+#!python
+from session import Session
+
+def main():
+    session = Session()
+    session.print_update()
+    session.timer()
 
 if __name__ == '__main__':
     main()
--- session.py
+++ session.py
@@ -1,13 +1,15 @@
 #!python
 import time
 import sys
+from tkinter import messagebox
+import random
 
 class Session(object):
 
     def __init__(self):
         self.subject = input('Enter the subject you\'re working on today: ')
         self.pomodoro = 0
-        self.hours = 0
+        self.hours = 0.0
 
     def print_update(self):
         """Print Current pomodoro's and subject."""
@@ -18,8 +20,34 @@ class Session(object):
         while i < self.pomodoro:
             print('|', end='')
             i += 1
+        print('')
+
+    def print_dialogue(self, tim):
+        if tim == 0:
+            random = random.randint(0,len(short_dia) - 1)
+            print('Take a short break!')
+            messagebox.showinfo("Break time!", "5 minute break: " + short_dia[random])
+        else:
+            random = random.randint(0,len(long_dia) - 1)
+            print('Take a long break!')
+            messagebox.showinfo("Break time!", "30 minute break: " + long_dia[random])
+
+    def rest(self, tim):
+        self.print_dialogue(tim)
+        enter = True
+        while enter == True:
+            enter = input("Press [Enter] when you\'re ready to focus again.")
 
     def update(self):
+        self.pomodoro += 1
+        self.hours += 0.5
+        if self.pomodoro > 3:
+            self.pomodoro = 0
+            self.rest(1)
+        else:
+            self.rest(0)
+        self.print_update()
+        return self.timer()
 
     def timer(self):
         """Keep track of 25 minute intervals"""
@@ -27,7 +55,7 @@ class Session(object):
         seconds = 0
         minutes = 0
 
-        while minutes < 25:
+        while seconds < 25:
             try:
                 sys.stdout.flush()
                 time.sleep(1)
@@ -35,6 +63,6 @@ class Session(object):
                 if seconds >= 60:
                     minutes += 1
                     seconds = 0
-            except KeyboardInterrupt, e:
-                break
+            except KeyboardInterrupt:
+                raise KeyError('You have finished the session.')
         return self.update()
